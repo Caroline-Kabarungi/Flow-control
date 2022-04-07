673ee2b61835f1467fef5b71383f12bcbf15554c
Caroline-Kabarungi <josephkabarungicaroline@gmail.com>
2022-04-07 22:52:20 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,49 @@
+fun main(){
+    odd()
+    robots(4)
+    robots(7)
+    robots(18)
+    robots(30)
+    //num()
+
+}
+// create a function that prints out all the odd numbers between 1 and 1oo.
+fun odd(){
+    for (z in 1..100)
+        if(z %2 !=0){
+            println(z)
+
+        }
+     }
+//create a function that take in an array of names and returns the number of
+// names longer than 5 characters
+fun name(names:Array<String>):Int{
+    var number=0
+    names.forEach{b->
+        if(b.length>5)
+            number++
+
+    }
+    return number
+}
+fun robots(age:Int){
+    if(age<6){
+        println("A glass of milk")
+    }
+    else if(age>=6 && age<15){
+        println("A bottle of coca cola")
+    }
+    fun num(){
+        for(h in 1.. 100)
+            if(h % 3==0 && h %5==0){
+            println("Fiz Buzz")
+            }else if(h %3==0){
+                println("Fiz")
+            }else{
+             if(h%5==0){
+                 println("Buz")
+
+             }
+            }
+    }
+}
